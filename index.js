dec0105d3dfa364bb07b934b9303ae86a7d6edfd
erica-y-chen <eyufanchen@gmail.com>
2019-04-25 09:45:55 -0700

    Added dynamic port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./api/server.js');
 
-server.listen(5000, () => {
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:5000 ***\n');
 });
